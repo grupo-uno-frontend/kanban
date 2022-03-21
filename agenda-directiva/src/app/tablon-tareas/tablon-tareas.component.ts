afe77e8ac36a78f357b1b9d3e515e81d620cbbb8
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-21 12:17:17 +0100

    a

--- agenda-directiva/src/app/tablon-tareas/tablon-tareas.component.ts
+++ agenda-directiva/src/app/tablon-tareas/tablon-tareas.component.ts
@@ -31,33 +31,31 @@ export class TablonTareasComponent implements OnInit {
           this.tareasDisponibles = tareas;
         })
       }
- /*LO DE ARRASTRAR dragStart(event,product: Product) {
-    this.draggedProduct = product;
+ dragStart(event,tarea: ITarea) {
+    this.draggedTarea = tarea;
 }
 
  drop(event) {
-    if (this.draggedProduct) {
-        let draggedProductIndex = this.findIndex(this.draggedProduct);
-        this.selectedProducts = [...this.selectedProducts, this.draggedProduct];
-        this.availableProducts = this.availableProducts.filter((val,i) => i!=draggedProductIndex);
-        this.draggedProduct = null;
+    if (this.draggedTarea) {
+        let draggedTareaIndex = this.findIndex(this.draggedTarea);
+        this.tareasSeleccionadas = [...this.tareasSeleccionadas, this.draggedTarea];
+        this.tareasDisponibles = this.tareasDisponibles.filter((val,i) => i!=draggedTareaIndex);
+        this.draggedTarea = null;
     }
 }
 dragEnd(event) {
-  this.draggedProduct = null;
+  this.draggedTarea = null;
 }
 
-findIndex(product: Product) {
+findIndex(tarea: ITarea) {
   let index = -1;
-  for(let i = 0; i < this.availableProducts.length; i++) {
-      if (product.id === this.availableProducts[i].id) {
+  for(let i = 0; i < this.tareasDisponibles.length; i++) {
+      if (tarea.id === this.tareasDisponibles[i].id) {
           index = i;
           break;
       }
   }
   return index;
 }
-*/
-
 }
 
