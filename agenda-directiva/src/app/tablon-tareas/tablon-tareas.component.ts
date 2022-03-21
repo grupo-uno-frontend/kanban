19fec07ad16fe6f563c1bc44a2c8074a8900c152
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-21 14:35:12 +0100

    3

--- agenda-directiva/src/app/tablon-tareas/tablon-tareas.component.ts
+++ agenda-directiva/src/app/tablon-tareas/tablon-tareas.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TareasService } from '../services/tareas.service';
 import { ITarea } from './tarea.interface';
-import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-tablon-tareas',
