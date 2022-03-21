63e0afe4b45e3d09137e7f64441429e23611b584
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-21 11:19:03 +0100

    tarea service finalizado

--- agenda-directiva/src/app/services/tareas.service.ts
+++ agenda-directiva/src/app/services/tareas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
-import { addDoc, collection, deleteDoc, doc, limit, orderBy, query } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, limit, orderBy, query, setDoc } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 import { ITarea } from '../tablon-tareas/tarea.interface';
 
@@ -14,6 +14,10 @@ export class TareasService {
     const tareas = collection(this.firestore, 'tareas');
     const realizadas = query(tareas, orderBy('fechaHora', 'asc'), limit(20));//desc
     return collectionData(realizadas, { idField: 'id' }) as Observable<ITarea[]>;}
+
+  updateTarea(tarea: ITarea) {
+    const tareaActualizar = doc(this.firestore, `tareas/${tarea.id}`);
+    return setDoc(tareaActualizar, tarea);}
 
   addTarea(tarea: ITarea) {
     const tareas = collection(this.firestore, 'tareas');
