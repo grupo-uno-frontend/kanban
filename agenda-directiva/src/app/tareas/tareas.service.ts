0822fc1cee8e134d072b0e653a50e24532713257
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-21 10:15:22 +0100

    hola

--- agenda-directiva/src/app/tareas/tareas.service.ts
+++ agenda-directiva/src/app/tareas/tareas.service.ts
@@ -12,7 +12,7 @@ export class TareasService {
   constructor(private firestore: Firestore) { }
   getTareas(): Observable<ITarea[]> {
     const tareas = collection(this.firestore, 'tareas');
-    const realizadas = query(tareas, orderBy('fechaHora', 'desc'), limit(20));
+    const realizadas = query(tareas, orderBy('fechaHora', 'asc'), limit(20));//desc
     return collectionData(realizadas, { idField: 'id' }) as Observable<ITarea[]>;}
 
 
