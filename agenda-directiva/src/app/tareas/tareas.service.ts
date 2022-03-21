d4db3ae4c7709c8c31b8b01d234d5cae7ca9e39f
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-21 10:02:06 +0100

    get tareas

--- agenda-directiva/src/app/tareas/tareas.service.ts
+++ agenda-directiva/src/app/tareas/tareas.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection, limit, orderBy, query } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { ITarea } from '../tablon-tareas/tarea.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareasService {
+  firestore: any;
 
-  constructor() { }
+  constructor(firestore: Firestore) { }
+  getTareas(): Observable<ITarea[]> {
+    const tareas = collection(this.firestore, 'tareas');
+    const realizadas = query(tareas, orderBy('fechaHora', 'desc'), limit(20));
+    return collectionData(realizadas, { idField: 'id' }) as Observable<ITarea[]>;
 }
