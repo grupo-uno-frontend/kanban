276219d25de3cd33ccf21afdc722378c2141e012
grupo-uno-frontend <grupo.uno.frontend@gmail.com>
2022-03-22 10:27:38 +0100

    1

--- agenda-directiva/src/app/login/login.component.ts
+++ agenda-directiva/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
-/* import { Router } from '@angular/router'; */
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +10,14 @@ import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private fireAuth: Auth/* , private router: Router */) {}
+  constructor(private fireAuth: Auth, private router: Router) {}
 
   ngOnInit(): void {
   }
 
   async loginGoogle() {
     await signInWithPopup(this.fireAuth, new GoogleAuthProvider());
-    /* this.router.navigateByUrl('chat'); */
+    this.router.navigateByUrl('tareas');
   }
 
 }
